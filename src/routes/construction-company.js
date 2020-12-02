03ee6225ffd282004874c415727849349c2741f5
Ramiro <ramiro.angel@radiumrocket.com>
2020-12-02 00:36:51 -0300

    construction company: added some comments to the /routes/construction-company.js file and made some minor changes

--- src/routes/construction-company.js
+++ src/routes/construction-company.js
@@ -1,11 +1,22 @@
 const constructionCompany = require('../controllers/construction-company.js');
 const router = require('express').Router();
 
+//Create a new Construction Company
 router.post('/addNew', constructionCompany.create);
+
+//Update a Construction Company by id
 router.put('/updateById/:id_company', constructionCompany.update);
+
+//Delete a Construction Company by id
 router.delete('/deleteById/:id_company', constructionCompany.delete);
+
+//Retrieve all Construction Companies
 router.get('/getAll', constructionCompany.findAll);
+
+//Retrieve Construction Companies by ID
 router.get('/getById/:id_company', constructionCompany.findOne);
-router.get('/getByType/:cuit', constructionCompany.findName);
+
+////Retrieve Construction Companies by type (CUIT)
+router.get('/getByType/:cuit', constructionCompany.findCuit);
 
 module.exports = router;
