1b0f1898c7fadde7681f327ae99a27e4e07474cb
Ramiro <ramiro.angel@radiumrocket.com>
2020-12-02 01:12:00 -0300

    boyler-types: added the main index.js code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-console.log('Main');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 3000;
+const db = require('./models');
+const router = require('./routes');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+//Connect to the server
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(()=> {
+    console.log("Connected to the database");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database", err);
+    process.exit();
+  });
+
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
+
+app.use(router);
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
