1abfecece664cdc0bba7b5c66646f02f230fafa3
Ramiro <ramiro.angel@radiumrocket.com>
2020-12-02 00:44:13 -0300

    construction-company: changes in the main 'index.js' file -> erased the line it was in the row 5 and added the one that is now in the row 9

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = 3000;
-const dotenv = require('dotenv').config();
 const db = require('./models');
 const router = require('./routes');
 
 app.use(bodyParser.json());
-
+app.use(bodyParser.urlencoded({ extended: true}));
 //Connect to the server
 db.mongoose
   .connect(db.url, {
@@ -20,14 +19,13 @@ db.mongoose
   .catch(err => {
     console.log("Cannot connect to the database", err);
     process.exit();
-  })
+  });
 
 app.get('/', (req, res) => {
   res.send('Hello world!');
 });
 
 app.use(router);
-
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
