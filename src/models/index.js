e6b759bb133c0ea7d4301097063adb4fcc483eec
Ramiro <ramiro.angel@radiumrocket.com>
2020-12-02 00:28:09 -0300

    construction-company: added the MongoDB url to the /model/index.js

--- src/models/index.js
+++ src/models/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const db = {};
 db.mongoose = mongoose;
-db.url = process.env.mongoURL;
+db.url = 'mongodb+srv://Admin:RwM2CTiylkTtGvxg@cluster0.l9taq.mongodb.net/CaldAR_M5?retryWrites=true&w=majority'
 db.technicians = require('./construction-company.js')(mongoose);
 
 module.exports = db;
